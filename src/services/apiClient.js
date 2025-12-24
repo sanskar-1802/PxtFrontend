06ffee0b79c8bd52f889014853e05a3e0ee1f2e4
sanskar-1802 <sanskar18092004@gmail.com>
2025-12-24 16:50:28 +0530

    deployed

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,8 +1,26 @@
+// import axios from "axios";
+
+// const apiClient = axios.create({
+//   baseURL: "http://localhost:5000/api",
+//   withCredentials: true,
+// });
+
+// // Automatically include Authorization token on every request
+// apiClient.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("accessToken");
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
+// export default apiClient;
+
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:5000/api",
-  withCredentials: true,
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  withCredentials: true,   // keep if you really need cookies
 });
 
 // Automatically include Authorization token on every request
@@ -15,3 +33,4 @@ apiClient.interceptors.request.use((config) => {
 });
 
 export default apiClient;
+
